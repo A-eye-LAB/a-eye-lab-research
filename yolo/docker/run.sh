630b879a61d6ca8bf6bc2bcce0f66561367a02a6
hu-nie <ehdgns8227@naver.com>
2024-11-12 13:37:53 +0000

    feat: 도커 실행 스크립트 추가

--- yolo/docker/run.sh
+++ yolo/docker/run.sh
@@ -2,6 +2,7 @@ docker build -t cataract:v0.1 .
 
 docker run --gpus all -itd \
   --ipc=host \
-  -v /home/hoon/cataract:/workspace/data \
+  -v /home/hoon/a-eye-lab-research:/workspace/a-eye-lab-research \
+  -v /home/hoon/cataract_data:/workspace/data \
   --name cataract_container \
   cataract:v0.1
