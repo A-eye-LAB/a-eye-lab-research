00f5d5219f0b5e8e6a2726eb59f1df088fdb0437
hu-nie <edgns8227@naver.com>
2025-07-31 00:39:28 +0000

    도커 삭제

--- docker/run.sh
+++
@@ -1,6 +0,0 @@
-docker run --gpus all -itd \
-  --ipc=host \
-  -v /home/hoon/a-eye-lab-research:/workspace/a-eye-lab-research \
-  -v /home/hoon/cataract_data:/workspace/data \
-  --name onnx_container \
-  onnx-gpu
